f376c25fbf2b89de05f606545740d36bd45a7d26
Fabio Adani <fabio.adani@ynap.com>
2018-01-31 12:54:31 +0100

    USe sqlite as storage

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const articles = [{ title : 'Example'}];
 const bodyParser = require('body-parser');
+const Article = require('./db.js').Article;
 
 app.set('port', process.env.port || 3000);
 
@@ -9,27 +10,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/articles', (req, res, next) => {
-    res.send(articles);
+    Article.all((err, articles) => {
+        if (err) return next(err);
+        res.send(articles);
+    });
 });
 
 app.post('/articles', (req, res, next) => {
-    const article = {title: req.body.title};
+    const article = {
+        title: req.body.title,
+        content: 'Sample Content'
+    };
     console.log(`Adding new article: ${article}`);
-    articles.push(article);
-    res.send(article);    
+    Article.create(article, (err, article) => {
+        if (err) return next(err);
+        res.send(article);
+    }); 
 });
 
 app.get('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log(`Fetching ${id}`);    
-    res.send(articles[id]);
+    Article.find(id, (err, article) => {
+        if (err) return next(err);
+        res.send(article);
+    });
 });
 
 app.delete('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log(`Deleting  ${id}`);    
-    delete articles[id];
-    res.send('Deleted');
+    Article.delete(id, (err) => {
+        if (err) return next(err);
+        res.send({ message: 'Deleted' });
+    });
 });
 
 app.listen(app.get('port'), () => console.log(`Express web app at localhost: ${app.get('port')}`));
