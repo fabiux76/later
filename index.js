113892fada9ca29a177392d5d4e822d8832ecf2d
Fabio Adani <fabio.adani@ynap.com>
2018-01-31 11:45:54 +0100

    Add routes and debug configuration

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const articles = [{ title : 'Example'}];
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.set('port', process.env.port || 3000);
+
+app.get('/articles', (req, res, next) => {
+    res.send(articles);
 });
 
-app.listen(port, () => console.log(`Express web app at localhost: ${port}`));
+app.post('/articles', (req, res, next) => {
+    res.send('OK');
+});
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log(`Fetching ${id}`);    
+    res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log(`Deleting  ${id}`);    
+    delete articles[id];
+    res.send('Deleted');
+});
+
+app.listen(app.get('port'), () => console.log(`Express web app at localhost: ${app.get('port')}`));
+
+module.exports = app;
